c725c4da9dd149197787947e248a9a499093fa67
dcg2022 <100590541+dcg2022@users.noreply.github.com>
2022-08-10 18:48:05 -0400

    Created buttonFunctions

---
+++ Team Project shooter/Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    /*
+    public void resume()
+    {
+        
+        if (gameManager.instance.isPaused)
+        {
+            gameManager.instance.isPaused = !gameManager.instance.isPaused;
+            gameManager.instance.cursorUnlockUnpause();
+
+        }
+    }
+
+
+    public void respawn()
+    {
+        gameManager.instance.playerScript.resetHP();
+        gameManager.instance.playerScript.respawn();
+        gameManager.instance.menuCurrentlyOpen.SetActive(false);
+
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void restart()
+    { 
+        gameManager.instance.cursorUnlockUnpause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+       
+
+    }
+
+
+
+    */
+}
